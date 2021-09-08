0cbae5f93aee365524841d6469a62f9f81b6fbf9
marcoalvesalmeida <marco.ifnmg@gmail.com>
2021-09-08 11:38:15 -0300

    Welcome text

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,8 @@ const App: React.FC = () => {
     <div className="App">
       <header className="App-header">
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Welcome to <code>Concierge Blog</code> in React.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
