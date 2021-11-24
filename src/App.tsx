e3014380ee17ca843fd066c278ec3227cb70cb83
NandaPersa <anandapersa@gmail.com>
2021-11-24 20:23:42 -0300

    add key in the component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { ThemeProvider } from "styled-components";
-import Principal from "./pages/Principal";
+import Principal from "./pages/Main";
 import { GlobalStyle } from "./Styles/Global";
 import theme from "./Styles/theme";
 
