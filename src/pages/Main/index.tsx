fd158a78a07d8a4ecdeed71793d9d53026d61867
raisa-design <almeida.alves.raisa@gmail.com>
2022-07-09 20:09:55 -0300

    remove link

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect } from "react";
-import { Link } from "react-router-dom";
 import Authors from "../../components/Authors";
 import Carrossel from "../../components/Carrossel";
 import MoreViews from "../../components/MoreViews";
